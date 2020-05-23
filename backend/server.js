59069a44ed67635eb00f0b3d1af6d38c8ea704d0
thisen@gmail.com <thisen@gmail.com>
2020-05-23 11:37:41 +0200

    Installed dependencies, added mongoose model and test endpoint

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,27 @@ import express from 'express'
 import bodyParser from 'body-parser'
 import cors from 'cors'
 import mongoose from 'mongoose'
+import crypto from 'crypto'
+import bcrypt from 'bcrypt-nodejs'
+
+const User = mongoose.model('User', {
+  name:{
+    type: String,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  accessToken: {
+    type: String,
+    default: () => crypto.randomBytes(128).toString('hex')
+  }
+})
+
+// Creating one user and apply hash on users password and store this hash insteda of the password
+const user = new User({name: 'Bob', password: bcrypt.hashSync('hello world')})
+user.save()
 
 const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/authAPI"
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
@@ -14,6 +35,17 @@ mongoose.Promise = Promise
 const port = process.env.PORT || 8080
 const app = express()
 
+// Authenticator function, later used on line 57 to only accept authorized user to the enpoint example.
+const authenticateUser = async (req, res, next) =>{
+  const user = await User.findOne({accessToken: req.header('Authorization')})
+  if(user){
+    req.user = user
+    next()
+  } else {
+    res.status(401).json({loggetOut: true})
+  }
+}
+
 // Add middlewares to enable cors and json body parsing
 app.use(cors())
 app.use(bodyParser.json())
@@ -21,6 +53,25 @@ app.use(bodyParser.json())
 // Start defining your routes here
 app.get('/', (req, res) => {
   res.send('Hello world')
+})
+
+// Example of checking if user is already authenticated
+app.post('/tweets', authenticateUser)
+app.post('/tweets', async (req, res) => {
+  // This will only happen if line 57 is true. 
+})
+
+// Validate user trying to log in
+app.post('/sessions', async (req, res) => {
+  const user = await User.findOne({name: req.body.name})
+
+  if(user && bcrypt.compareSync(req.body.password, user.password)) {
+    // Success
+    res.json({userId: user._id, accessToken: user.accessToken})
+  } else {
+    // Failure
+    res.json({notFound: true})
+  }
 })
 
 // Start the server
